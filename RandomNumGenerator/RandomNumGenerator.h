a937324bf05c887f0a579b4c7bcf4b28da77b7aa
Mr-lhk <mr_lhk@163.com>
2025-03-11 23:49:01 +0800

    添加项目文件。

---
+++ RandomNumGenerator/RandomNumGenerator.h
@@ -0,0 +1,32 @@
+﻿
+// RandomNumGenerator.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CRandomNumGeneratorApp:
+// 有关此类的实现，请参阅 RandomNumGenerator.cpp
+//
+
+class CRandomNumGeneratorApp : public CWinApp
+{
+public:
+	CRandomNumGeneratorApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CRandomNumGeneratorApp theApp;
